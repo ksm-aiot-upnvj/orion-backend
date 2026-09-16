28fcd63a4beb7a3be2f8deb1bc6a1ab466971dca
KSM-AIoT <aiot.upnvj@gmail.com>
2026-09-16 21:17:17 +0700

    fix: missing CDN on Content-Security-Policy

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ async def add_security_headers(request: Request, call_next):
         response.headers["Content-Security-Policy"] = (
             "default-src 'self'; "
             "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; "
             "font-src 'self' https://fonts.gstatic.com; "
             "img-src 'self' data: https: blob:; "
             "connect-src 'self' http://localhost:8000 http://127.0.0.1:8000 https://*; "
